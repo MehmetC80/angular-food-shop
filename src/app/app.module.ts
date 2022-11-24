55a89558fff72da29d94eaa4a8044e194878c06a
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-11-24 13:52:01 +0100

    add food-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, TagsComponent, FoodPageComponent],
   imports: [BrowserModule, AppRoutingModule, RatingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/pages/food-page/food-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/food';
+
+@Component({
+  selector: 'app-food-page',
+  templateUrl: './food-page.component.html',
+  styleUrls: ['./food-page.component.scss'],
+})
+export class FoodPageComponent implements OnInit {
+  food!: Food;
+
+  constructor(
+    activatedRoute: ActivatedRoute,
+    private foodService: FoodService
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.id) {
+        this.food = foodService.getFoodById(params.id);
+      }
+    });
+  }
+
+  ngOnInit(): void {}
+}
